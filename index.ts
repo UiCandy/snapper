b8e559b316cbc94c725a3eb3e6b2a3bc5ebaac32
Rishav Sharma <rishav.sharma@ada.com>
2023-02-24 10:48:14 +0530

    fix: updated port

--- index.ts
+++ index.ts
@@ -6,53 +6,75 @@ import puppeteer from "puppeteer";
 const app = express();
 
 const pupeteerHandler = handler({
-	resolve: async (params: any) => {
-		const browser = await puppeteer.launch({
-			headless: false, //  debug mode
-			defaultViewport: null, //Defaults to an 800x600 viewport
-			userDataDir: "./user_data",
-		});
-		try {
-			const context = browser.defaultBrowserContext();
-			const page = await browser.newPage();
+  resolve: async (params: any) => {
+    const browser = await puppeteer.launch({
+      headless: false, //  debug mode
+      defaultViewport: null, //Defaults to an 800x600 viewport
+      userDataDir: "./user_data",
+    });
+    try {
+      const context = browser.defaultBrowserContext();
+      const page = await browser.newPage();
 
-			await context.overridePermissions("https://tradingview.com", [
-				"clipboard-read",
-				"clipboard-write",
-			]);
+      await context.overridePermissions("https://tradingview.com", [
+        "clipboard-read",
+        "clipboard-write",
+      ]);
 
-			await page.goto(params.body.chart);
+      await page.goto(params.body.chart);
 
-			// check if page  has loaded
-			await page.waitForSelector(".first-_hkHmHWX");
-			await page.waitForSelector("#header-toolbar-screenshot");
+      // check if page  has loaded
+      await page.waitForSelector(".first-_hkHmHWX");
+      await page.waitForSelector("#header-toolbar-screenshot");
 
-			// click screenshot icon
-			await page.click("#header-toolbar-screenshot");
-			await page.click(
-				'div[data-name="copy-link-to-the-chart-image"] .labelRow-RhC5uhZw'
-			);
+      // click screenshot icon
+      await page.click("#header-toolbar-screenshot");
+      await page.click(
+        'div[data-name="copy-link-to-the-chart-image"] .labelRow-RhC5uhZw'
+      );
 
-			await page.waitForSelector(".container-success-Layw3R3g");
+      await page.waitForSelector(".container-success-Layw3R3g");
 
-			const copiedText = await page.evaluate(() =>
-				navigator.clipboard.readText()
-			);
+      const copiedText = await page.evaluate(() =>
+        navigator.clipboard.readText()
+      );
 
-			return copiedText;
-		} catch (error) {
-			console.error(error);
-		} finally {
-			await browser.close();
-		}
-	},
+      return copiedText;
+    } catch (error) {
+      console.error(error);
+    } finally {
+      await browser.close();
+    }
+  },
+});
+
+const helloHandler = handler({
+  resolve: async () => {
+    const browser = await puppeteer.launch({
+      headless: true,
+    });
+
+    const page = await browser.newPage();
+    await page.goto("https://example.com");
+
+    try {
+      const element = await page.waitForSelector("div > p"); // select the element
+      if (element) {
+        const text: any = await element.evaluate((el) => el.textContent); // grab
+        return text;
+      }
+    } catch (e) {
+      console.error(e);
+    }
+  },
 });
 
 const routes = {
-	chart: pupeteerHandler,
+  chart: pupeteerHandler,
+  hello: method({ GET: helloHandler }),
 };
 app.use(cors());
 app.use("", initBridge({ routes }).expressMiddleware());
-app.listen(8081, () => {
-	console.log(`Listening on port 8081`);
+app.listen(8080, () => {
+  console.log(`Listening on port 8080`);
 });
