1d0b4d9335044978a3a26fab33425ea422a99642
Rishav Sharma <rishav.sharma@ada.com>
2023-02-24 20:38:27 +0530

    fix: add files for scr

--- index.ts
+++ index.ts
@@ -8,38 +8,24 @@ const app = express();
 const pupeteerHandler = handler({
   resolve: async (params: any) => {
     const browser = await puppeteer.launch({
-      headless: false, //  debug mode
+      headless: true, //  debug mode
       defaultViewport: null, //Defaults to an 800x600 viewport
       userDataDir: "./user_data",
     });
     try {
-      const context = browser.defaultBrowserContext();
       const page = await browser.newPage();
-
-      await context.overridePermissions("https://tradingview.com", [
-        "clipboard-read",
-        "clipboard-write",
-      ]);
-
+      const ticker = params.body.chart.split("%3A")[1].split("&")[0];
       await page.goto(params.body.chart);
 
       // check if page  has loaded
       await page.waitForSelector(".first-_hkHmHWX");
       await page.waitForSelector("#header-toolbar-screenshot");
 
-      // click screenshot icon
-      await page.click("#header-toolbar-screenshot");
-      await page.click(
-        'div[data-name="copy-link-to-the-chart-image"] .labelRow-RhC5uhZw'
-      );
+      await page.screenshot({
+        path: `./data/${ticker}_screenshot.jpg`,
+      });
 
-      await page.waitForSelector(".container-success-Layw3R3g");
-
-      const copiedText = await page.evaluate(() =>
-        navigator.clipboard.readText()
-      );
-
-      return copiedText;
+      return ticker;
     } catch (error) {
       console.error(error);
     } finally {
