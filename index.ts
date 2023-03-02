388b025a1930b8cc393f6229a42ec4982cb4631e
Rishav Sharma <rishav.sharma@ada.com>
2023-03-02 18:44:28 +0530

    fix: remove user data

--- index.ts
+++ index.ts
@@ -15,7 +15,6 @@ const pupeteerHandler = handler({
       defaultViewport: null, //Defaults to an 800x600 viewport
       userDataDir: "./user_data",
       devtools: false,
-      args: ["--no-sandbox", "--disable-setuid-sandbox"],
     });
 
     try {
